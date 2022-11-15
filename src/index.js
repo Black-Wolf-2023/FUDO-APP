51ad920085400715e726dc91c35b64fb8f007397
Black-Wolf-2022 <abdo.eslam2021@gmail.com>
2022-11-15 11:15:51 +0200

    Puhs

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./components/app/App";
-import { Provider } from "react-redux";
-import { store } from "./redux/store/store";
+import  ReactDOM  from "react-dom/client";
+import { App } from "./component/App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-   <Provider store={store}>
-     <App />
-   </Provider>
-);
+    <App/>
+)
--- src/redux/store/store.js
+++
@@ -1,5 +0,0 @@
-import { applyMiddleware, createStore } from "redux";
-import { Get__data } from "../reducers/Get__data";
-import thunk from "redux-thunk";
-
-export const store = createStore(Get__data,applyMiddleware(thunk));
